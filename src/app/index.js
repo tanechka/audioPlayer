0965e0fc16010d7bb23867db87f1dd2d6350f045
Radyuk Tatyana <0400739110>
2018-09-25 17:24:32 +0300

    start of development of an audio player

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,31 @@
 import 'normalize-stylus';
 import './main.styl';
+
+class AudioPlayer {
+    constructor() {
+        this.listFiles = [];
+        this.player = document.querySelector('.js-player');
+        this.inputSelectFiles = document.querySelector('.js-files');
+    };
+
+    addEventChange() {
+        const self = this;
+
+        this.inputSelectFiles.addEventListener('change', function(input) {
+            self.renderListAudio(input.currentTarget.files);
+        });
+    }
+
+    renderListAudio(listFiles) {
+        this.player.src = window.URL.createObjectURL(listFiles[0]);
+        this.player.play();
+    }
+
+    init() {
+        this.addEventChange();
+    }
+};
+
+const audioPlayer = new AudioPlayer();
+
+audioPlayer.init();
