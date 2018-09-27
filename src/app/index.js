388dc15cd591abbcd093615ea148b34d24fe3cec
Tanya Radyuk <gvardi0@gmail.com>
2018-09-27 12:49:32 +0300

    WIP

--- src/app/index.js
+++ src/app/index.js
@@ -3,38 +3,89 @@ import './main.styl';
 
 class AudioPlayer {
     constructor() {
-        this.player = document.querySelector('.js-player');
+        this.audio = new Audio();
         this.inputSelectFiles = document.querySelector('.js-files');
         this.listSection = document.querySelector('.js-list');
+        this.playButton = document.querySelector('.js-play');
+        this.pauseButton = document.querySelector('.js-pause');
+        this.nextButton = document.querySelector('.js-next');
+        this.prevButton = document.querySelector('.js-prev');
+        this.statusAudio = document.querySelector('.js-playing');;
     };
 
     addEventChange() {
-        const self = this;
-
-        this.inputSelectFiles.addEventListener('change', function(input) {
-            self.renderListAudio(input.currentTarget.files);
+        this.inputSelectFiles.addEventListener('change', (input) => {
+            this.setActionPlayer(input.currentTarget.files);
         });
+    }
+
+    setActionPlayer(files) {
+        this.renderListAudio(files);
+        this.initPlayingItem();
     }
 
     renderListAudio(listFiles) {
-        console.log(listFiles.name);
-        console.log(listFiles);
-        const self = this;
-        let arr = [];
+        let listAudio = [];
 
         for (let file of listFiles) {
-            let template = `<li data-file="${window.URL.createObjectURL(file)}">${file.name}</li>`;
-            arr.push(template);
+            let template = this.createTemplate(file);
+            listAudio.push(template);
         }
 
-        this.listSection.innerHTML = arr.join('');
+        this.listSection.innerHTML = listAudio.join('');
+      }
 
-        this.listSection.addEventListener('click', function (event) {
-            self.player.src = event.target.dataset.file;
-            self.player.play();
-        });
+    initPlayingItem() {
+      //console.log(this.listSection)
+        this.setActiveItem(this.listSection.firstChild);
+      //  this.listSection.firstChild
+        // this.listSection.addEventListener('click', (event) => {
+        //   this.setActiveItem(event.target);
+        // });
     }
 
+    setActiveItem(itemToPlay) {
+        this.audio.src = itemToPlay.dataset.file;
+        this.removeActiveClass();
+        itemToPlay.classList.add('js-playing');
+        this.audio.play();
+    }
+
+    removeActiveClass() {
+        this.statusAudio && this.statusAudio.classList.remove('js-playing');
+    }
+
+    createTemplate(file) {
+        return `<li data-file="${window.URL.createObjectURL(file)}">${file.name}</li>`;
+    }
+
+            // let audio = new Audio();
+            //
+            // this.listSection.addEventListener('click', function (event) {
+            //     document.querySelector('.js-playing') !== null && document.querySelector('.js-playing').classList.remove('js-playing');
+            //     audio.src = event.target.dataset.file;
+            //     event.target.classList.add('js-playing');
+            // });
+            //
+            // this.play.addEventListener('click', function (event) {
+            //     audio.play();
+            // });
+            //
+            // this.pause.addEventListener('click', function (event) {
+            //     audio.pause();
+            // });
+            //
+            // this.nextButton.addEventListener('click', function (event) {
+            //     audio.src = document.querySelector('.js-playing').nextSibling.dataset.file;
+            //     document.querySelector('.js-playing') !== null && document.querySelector('.js-playing').classList.remove('js-playing');
+            //     audio.play();
+            // });
+            //
+            // this.prevButton.addEventListener('click', function (event) {
+            //     document.querySelector('.js-playing') !== null && document.querySelector('.js-playing').classList.remove('js-playing');
+            //     audio.src = document.querySelector('.js-playing').prevSibling.dataset.file;
+            //     audio.play();
+            // });
     init() {
         this.addEventChange();
     }
